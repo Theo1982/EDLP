37f56c56018acbb8816852d2c837735deb297362
Theo1982 <etereoeternauta@gmail.com>
2022-06-07 13:48:42 -0300

    Add files via upload
    TP8

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function copy() {
+  alert("Autor: Octavio A. Macchi. EMAIL: octavio_macchi@hotmail.com");
+}
+
+function inclusive()
+    {
+        document.body.style.backgroundColor= "black";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.color = "#90ffe9";
+        document.getElementById("nav").style.backgroundColor = "whitesmoke";
+        document.getElementById("nav").style.color = "black";
+        document.getElementById("section").style.backgroundColor = "black";
+        document.getElementById("section").style.color = "whitesmoke";
+        document.getElementById("aside").style.backgroundColor = "black";
+        document.getElementById("footer").style.backgroundColor = "black";
+    }
